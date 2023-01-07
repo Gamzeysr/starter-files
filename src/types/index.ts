a1f864da5167404b0d98139ce21fa5a6616856d9
Gamze <gamzeyasar35@icloud.com>
2023-01-07 15:53:54 +0300

    index

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,39 @@ export type optionCityType = {
     lon: number;
 
 };
+
+export type forecastTyper = {
+    name: string;
+    country: string;
+    sunrise: number;
+    sunset: number;
+    list: [
+        {
+            dt: number;
+            main: {
+                feels_like: number;
+                humidty: number;
+                pressure: number;
+                temp: number;
+                temp_max: number;
+                temp_min: number;
+            };
+            weather: [
+                {
+                    main: string;
+                    icon: string;
+                    description: string;
+                }
+            ];
+            wind: {
+                speeed: number;
+                deg: number; gust: number;
+            };
+            clouds: {
+                all: number;
+
+            };
+            visibility: number;
+        }
+    ]
+}
